282e0ffd85471c9a7142217119193f45eee1f140
桂素伟 <axzxs2001@163.com>
2017-11-08 15:01:31 +0800

    自定义策略验证。

--- ProjectTemplate/AspNetCoreTemplate/Controllers/HomeController.cs
+++ ProjectTemplate/AspNetCoreTemplate/Controllers/HomeController.cs
@@ -8,9 +8,15 @@ using AspNetCoreTemplate.Models;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
 using AspNetCoreTemplate.Models.Repository;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
 
 namespace AspNetCoreTemplate.Controllers
 {
+
+    [Authorize(Policy = "Permission")]//【4自定义策略Cookie验证】与固定角色Cookie验证二选一
     public class HomeController : Controller
     {
         /// <summary>
@@ -26,7 +32,7 @@ namespace AspNetCoreTemplate.Controllers
         {
             //【3NLog】日志对象
             _logger = logger;
-         
+
         }
 
         public IActionResult Index()
@@ -53,5 +59,65 @@ namespace AspNetCoreTemplate.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        #region 【4自定义策略Cookie验证】与固定角色Cookie验证二选一
+        [AllowAnonymous]
+        [HttpGet("login")]
+        public IActionResult Login(string returnUrl = null)
+        {
+            TempData["returnUrl"] = returnUrl;
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(string userName, string password, string returnUrl = null)
+        {
+            //这个集合模拟用户表，可从数据库中查询出来
+            var list = new List<dynamic> {
+                new { UserName = "gsw", Password = "111111", Role = "admin",Name="桂素伟"},
+                new { UserName = "aaa", Password = "222222", Role = "system",Name="测试A"}
+            };
+            var user = list.SingleOrDefault(s => s.UserName == userName && s.Password == password);
+            if (user != null)
+            {
+                //用户标识
+                var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+                //如果是基于角色的授权策略，这里要添加用户
+                identity.AddClaim(new Claim(ClaimTypes.Name, user.UserName));
+                //如果是基于角色的授权策略，这里要添加角色
+                identity.AddClaim(new Claim(ClaimTypes.Role, user.Role));
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(identity));
+                if (returnUrl == null)
+                {
+                    returnUrl = TempData["returnUrl"]?.ToString();
+                }
+                if (returnUrl != null)
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    return RedirectToAction(nameof(HomeController.Index), "Home");
+                }
+            }
+            else
+            {
+                const string badUserNameOrPasswordMessage = "用户名或密码错误！";
+                return BadRequest(badUserNameOrPasswordMessage);
+            }
+        }
+        [HttpGet("logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
+        }
+        [AllowAnonymous]
+        [HttpGet("denied")]
+        public IActionResult Denied()
+        {
+            return View();
+        }
+        #endregion
     }
 }
--- ProjectTemplate/AspNetCoreTemplate/Startup.cs
+++ ProjectTemplate/AspNetCoreTemplate/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Http;
+using AuthorizePolicy;
+using Microsoft.AspNetCore.Authorization;
 
 namespace AspNetCoreTemplate
 {
@@ -26,10 +28,11 @@ namespace AspNetCoreTemplate
             var connectionString1 = Configuration.GetConnectionString("ConnectionString1");
             var connectionString2 = Configuration.GetConnectionString("ConnectionString2");
 
-            //【2固定角色Cookie验证】 注入验证 2.0
+
+            #region 【2固定角色Cookie验证】 注入验证 2.0
             services.AddAuthentication(options =>
             {
-                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;     
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
             })
             .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, m =>
             {
@@ -38,7 +41,31 @@ namespace AspNetCoreTemplate
                 m.LogoutPath = new PathString("/logout");
                 m.Cookie.Path = "/";
             });
+            #endregion
 
+            #region 【4自定义策略Cookie验证】与固定角色Cookie验证二选一
+            services.AddAuthorization(options =>
+            {
+                //这个集合模拟用户权限表,可从数据库中查询出来
+                var permission = new List<Permission> {
+                              new Permission {  Url="/", Name="admin"},
+                              new Permission {  Url="/home/permissionadd", Name="admin"},
+                              new Permission {  Url="/", Name="system"},
+                              new Permission {  Url="/home/contact", Name="system"}
+                          };
+                //如果第三个参数，是ClaimTypes.Role，上面集合的每个元素的Name为角色名称，如果ClaimTypes.Name，即上面集合的每个元素的Name为用户名
+                var permissionRequirement = new PermissionRequirement("/denied", permission, ClaimTypes.Role);
+                options.AddPolicy("Permission",
+                          policy => policy.Requirements.Add(permissionRequirement));
+            }).AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+            {
+                options.LoginPath = new PathString("/login");
+                options.AccessDeniedPath = new PathString("/denied");
+                options.ExpireTimeSpan = TimeSpan.FromSeconds(20);
+            });
+            //注入授权Handler
+            services.AddSingleton<IAuthorizationHandler, PermissionHandler>();
+            #endregion
 
             services.AddMvc();
         }
